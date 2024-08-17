ec795db35465a5a3a23c886f52201878b3c0b45f
Shah-Faisal-Khan <salahuddin.khan9952@gmail.com>
2024-08-18 01:33:56 +0530

    correction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './pages/main';
 import Work from './pages/work'; // Import the new Work page
 
